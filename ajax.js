7e3cf390f147afe535a35ffd55bbc7b3dcd45a1e
Budr <budr245@gmail.com>
2019-08-06 23:02:57 +0300

    Added spinners and all blocks

--- ajax.js
+++ ajax.js
@@ -1,14 +1,13 @@
 (()=>{
     $('#registration').submit(function(e) {
         let form = $(this);
-        console.log(form);
+       // console.log(form);
         $.ajax({
             type: form.attr('method'),
             url: form.attr('action'),
             data: form.serialize(),
-            success: function (response) {
-            console.log('success', response);
-            }
+            success: function (result) {
+                location.href = "./company.html"}
         })
     });
 })();
--- company.js
+++ company.js
@@ -1,23 +1,37 @@
-(async () => {
+( () => {
     'use strict';
+    let data;
     async function getCompany() {
         const response = await axios.get('http://codeit.ai/codeitCandidates/serverFrontendTest/company/getList')
             .then(function (response) {
                 console.log(response);
-                let tableBody = document.getElementById('nameCompanies');
-                for (let i = 0; i <= response.data.list.length; i++) {
-                    tableBody.innerHTML += `
-                 <tr>
-                <td>${response.data.list[i].name}</td>
-                 </tr>`;
-                }
+                data = response.data.list;
             })
             .catch(function (error) {
                 console.warn(error);
             })
     }
+    function renderTotalCompanies(response) {
+        let nodeTableText = '<table>';
+        let tableBody = document.getElementById('nameCompanies');
+        for (let i = 0; i < response.length; i++) {
+            nodeTableText += `
+                 <tr>
+                <td>${response[i].name}</td>
+                 </tr>`;
+        }
+        nodeTableText += '</table>';
+        tableBody.innerHTML=nodeTableText;
+    }
+    function renderCountCompanies(response) {
+        let circle = document.getElementById('circle');
+        circle.innerHTML = `${response.length}`;
+    }
+    setTimeout(async ()=> {
+        await getCompany();
+        await renderTotalCompanies(data);
+        await renderCountCompanies(data);
+    }, 1000);
 
-
-    const companies = await getCompany();
 
 })();
